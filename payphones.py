1cf68feef0a83edde57a1c8cb1c16a854662032f
golansha <golansha@gmail.com>
2023-05-17 11:08:30 +0300

    changing definition for sandwich according to correspondence with author

--- payphones.py
+++ payphones.py
@@ -24,7 +24,7 @@ def findMaxDistanceSet(arr, neighbor_left, neighbor_right, places):
         if distance > maximal_distance:
             maximal_distance = distance
             farthest = [i]
-    return farthest
+    return maximal_distance, farthest
 
 """ 
 finds the cells belonging to the longest interval/s
@@ -43,13 +43,14 @@ def find_max_interval(arr, neighbor_left, neighbor_right):
         if arr[i] != -1: continue
         left = max(-1, neighbor_left[i])
         right = min(len(arr), neighbor_right[i])
-        interval = right - left
+        interval = right - left - 1
+        if(isinf(neighbor_right[i]) or isinf(neighbor_left[i])): interval = interval*2-1
         if interval == max_interval_size:
             result.append(i)
         if interval > max_interval_size:
             max_interval_size = interval
             result = [i]
-    return result
+    return max_interval_size, result
 
 """ 
 counts the number of payphones permutations
@@ -72,23 +73,28 @@ preferLongInterval: bool
 def p1_rec(arr, neighbor_left, neighbor_right, nth, preferEdges, preferLongInterval):
     # Base case
     if nth == len(arr):
+        # print(arr)
         return 1
 
     # Find the set of cells that are equivalent candidates for the next user
     if nth == 0:
         candidates = range(len(arr))
-    elif preferEdges and (arr[0] == -1 or arr[-1] == -1):
-        candidates = [0, len(arr) - 1]
+    # elif preferEdges and (arr[0] == -1 or arr[-1] == -1):
+    #     candidates = [0, len(arr) - 1]
     elif preferLongInterval:
-        candidates = find_max_interval(arr, neighbor_left, neighbor_right)
+        max_interval_size, candidates = find_max_interval(arr, neighbor_left, neighbor_right)
     else:
         candidates = range(len(arr))
     result = 0
-    preferred = findMaxDistanceSet(arr, neighbor_left, neighbor_right, candidates)
-
+    maximal_distance, preferred = findMaxDistanceSet(arr, neighbor_left, neighbor_right, candidates)
+    # if (maximal_distance == 1) and (arr[0] == -1 or arr[-1] == -1) and preferEdges:
+    #     maximal_distance, preferred = findMaxDistanceSet(arr, neighbor_left, neighbor_right, [0, len(arr) - 1])
+    edgesVacant = (arr[0] == -1 or arr[-1] == -1)
     # Assign the next user and call recursively
     for i in preferred:
         # Assign an option
+        sandwiched = (neighbor_left[i]==i-1) and (neighbor_right[i]==i+1)
+        if (preferEdges and sandwiched and edgesVacant): continue
         arr[i] = nth
         left = max(-1, neighbor_left[i])
         right = min(len(arr), neighbor_right[i])
@@ -109,10 +115,25 @@ def p1_rec(arr, neighbor_left, neighbor_right, nth, preferEdges, preferLongInter
 
     return result
 
-
+def p(n,i):
+    if i==1:
+        return p1(n,False,False)
+    if i==2:
+        return p1(n,False,True)
+    if i==3:
+        return p1(n,True,False)
+    if i==4:
+        return p1(n,True,True)
 def p1(n, preferEdges, preferLongInterval):
     return p1_rec([-1] * n, [float('-inf')] * n, [float('inf')] * n, 0, preferEdges, preferLongInterval)
-
-
-for i in range(11):
-    print(f'p4({i}) = {p1(i, True, True)}')
+# print("sequences for p1:")
+# print(f'A total of {p1(7,False,False)} sequences')
+print("sequences for p2:")
+print(f'A total of {p1(5,True,True)} sequences')
+# print("sequences for p3:")
+# print(f'A total of {p1(7,True,False)} sequences')
+# print("sequences for p4:")
+# print(f'A total of {p1(7,True,True)} sequences')
+for j in range(1,5):
+    for i in range(11):
+         print(f'p{j}({i}) = {p(i,j)}')
